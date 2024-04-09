1980eacc9ca7000be13b816d77f562192eae71c8
Don Carlos Zendejas <czende@hotmail.com>
2024-04-09 11:54:23 -0600

    Update new endpoint primero IndexController.java

--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -4,9 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class IndexController {
     @GetMapping("/")
     public String index() {
         return "Hello World";
+    }
+
+    @GetMapping("/tutorials")
+    public String primero() {
+        return "Primer API";
     }
 }
